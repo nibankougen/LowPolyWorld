2992a91ffc84f9fd980d9463078198141c8ad121
nibankougen@gmail.com <kiyo_8into32@me.com>
2026-04-11 00:20:21 +0900

    paint-engine/target/をgitignoreに追加

--- paint-engine/target/debug/deps/paint_engine-061a65305e09ea7e.d
+++
@@ -1,5 +0,0 @@
-D:\Documents\Unity Projects\LowPolyWorld\paint-engine\target\debug\deps\paint_engine-061a65305e09ea7e.d: src\lib.rs
-
-D:\Documents\Unity Projects\LowPolyWorld\paint-engine\target\debug\deps\libpaint_engine-061a65305e09ea7e.rmeta: src\lib.rs
-
-src\lib.rs:
--- paint-engine/target/debug/deps/paint_engine-912586e6714c42ee.d
+++
@@ -1,5 +0,0 @@
-D:\Documents\Unity Projects\LowPolyWorld\paint-engine\target\debug\deps\paint_engine-912586e6714c42ee.d: src\lib.rs
-
-D:\Documents\Unity Projects\LowPolyWorld\paint-engine\target\debug\deps\libpaint_engine-912586e6714c42ee.rmeta: src\lib.rs
-
-src\lib.rs:
